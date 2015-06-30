13b6ad5e5e6658290677d581229464a38db1c794
Fisher Adelakin <temidayoadelakin@gmail.com>
2015-06-29 23:59:33 -0400

    sql is weird. // create database

--- app/src/main/java/com/fisheradelakin/traveltracker/DbHelper.java
+++ app/src/main/java/com/fisheradelakin/traveltracker/DbHelper.java
@@ -12,17 +12,42 @@ public class DbHelper extends SQLiteOpenHelper {
     private static final String DATABASE_NAME = "traveltracker.db";
     private static final int DATABASE_VERSION = 1;
 
-    public DbHelper(Context context) {
+    private static final String MEMORIES_TABLE = "memories";
+
+    private static final String COLUMN_LATITUDE = "latitude";
+    private static final String COLUMN_LONGITUDE = "longitude";
+    private static final String COLUMN_CITY = "city";
+    private static final String COLUMN_COUNTRY = "country";
+    private static final String COLUMN_NOTES = "notes";
+    private static final String COLUMN_ID = "_id";
+
+    private static DbHelper singleton = null;
+
+    public static DbHelper getInstance(Context context) {
+        if(singleton == null) {
+            singleton = new DbHelper(context.getApplicationContext());
+        }
+        return singleton;
+    }
+
+    private DbHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        db.execSQL("CREATE TABLE " + MEMORIES_TABLE + " ("
+                + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + COLUMN_LATITUDE + " DOUBLE, "
+                + COLUMN_LONGITUDE + " DOUBLE, "
+                + COLUMN_CITY + " TEXT, "
+                + COLUMN_COUNTRY + " TEXT, "
+                + COLUMN_NOTES + " TEXT)");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+        db.execSQL("DROP TABLE IF EXISTS " + MEMORIES_TABLE);
+        onCreate(db);
     }
 }
